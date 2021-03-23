8f770fad65e3fab485f86b7533e7609dc28c9961
hdm007 <harry.mardirossian13@gmail.com>
2021-03-22 23:22:37 -0400

    server set up, now create routes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+const KnexSessionStore = require('connect-session-knex')(session)
+
+const usersRouter = require('./users/users-router');
+const authRouter = require("./auth/auth-router");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -17,9 +22,30 @@ const cors = require("cors");
 
 const server = express();
 
+server.use(session({
+  name: 'chocolatechip',
+  secret: "this is a secret",
+  cookie: {
+    maxAge: 100 * 60 * 60,
+    secure: false,
+    httpOnly: false
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: require('../data/db-config'),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+}))
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
